7e33a4e1e654588673e54d0f4d9dab6718bcb2e9
Erikovich82 <60335825+Erikovich82@users.noreply.github.com>
2023-06-22 07:22:50 +0600

    Delete До вариантов товара/OnlineShop directory

--- До вариантов товара/OnlineShop/user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user','user_name','address','phone',
-                    'city','country','image_tag']
-
-admin.site.register(UserProfile, UserProfileAdmin)
